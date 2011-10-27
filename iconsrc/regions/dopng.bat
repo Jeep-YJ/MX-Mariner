f1c85fb28728e634a29d7ab66249fec54a16891d
Manimaul - Will Kamp <manimaul@gmail.com>
2011-10-27 06:43:18 -0700

    change to medium sized regions

---
+++ iconsrc/regions/dopng.bat
@@ -0,0 +1,39 @@
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region02.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region02.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region03.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region03.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region04.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region04.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region06.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region06.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region07.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region07.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region08.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region08.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region10.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region10.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region12.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region12.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region13.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region13.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region14.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region14.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region15.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region15.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region17.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region17.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region22.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region22.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region24.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region24.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region26.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region26.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region30.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region30.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region32.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region32.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region34.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region34.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region36.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region36.svg
+
+"C:\Program Files (x86)\Inkscape\inkscape.exe" --without-gui --export-png=region40.png --export-dpi=72 --export-background-opacity=0 --export-width=80 --export-height=80 region40.svg
